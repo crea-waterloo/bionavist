322ad8070be7f80bdc993cfb500f91c51b9b8725
David Song <David.Song0802@gmail.com>
2015-03-14 05:27:38 -0400

    websocket, up and running

--- server.js
+++ server.js
@@ -1,47 +1,129 @@
+// Libraries
 var http = require('http'),	
-	pg = require('pg'), 
 	fs = require('fs'), 
-	Router = require('node-simple-router');
+	JSONStream = require('JSONStream'), 
+	pg = require('pg'), 
+	QueryStream = require('pg-query-stream'), 
+	url = require('url'), 
+	through = require('through'),
+	Router = require('node-simple-router'), 
+	websocket = require('websocket-stream'),
+	nodejswebsocket = require('nodejs-websocket');
 
-var connParam = "postgres://creauser:crealogin@localhost/crearesearch";
+// Variables
+var connParam = "postgres://creauser:crealogin@localhost/crearesearch", 
+	httpPort = process.argv[2], 
+	router = new Router(), 
+	filePath = "static/index.html",
+	websocketStream;
 
-// var port = process.argv[2];
+// Webpage Routes
+router.get('/', function (request, response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
 
-// var router = new Router();
+	var readStream = fs.createReadStream(filePath);
+	readStream.pipe(response);
+});
 
-// router.get('/', function (request, response) {
+// Getting Javascript
+// .. kinda painful that I have to route this myself..
+router.get('/scripts/websocket-client.js', function (request, response) {
+	response.writeHead(200, {'Content-Type': 'text/javascript'});
 
-// });
+	var readStream = fs.createReadStream('static/scripts/websocket-client.js');
+	readStream.pipe(response);
+})
 
 // router.get('/api/get_all_relations', function (request, response) {
 
+// 	// Parsing URL
+// 	var parsedRequest = url.parse(request.url, true),
+// 		keyword = parsedRequest.query.keyword,
+// 		databaseQuery,
+// 		databaseQueryArray;
+
+// 	// Building Query
+// 	if (keyword == null) {
+// 		databaseQuery = "SELECT * FROM filtered_relations";
+// 		databaseQueryArray = [];
+// 	} else {
+// 		databaseQuery = "SELECT * FROM filtered_relations";
+// 		databaseQueryArray = [];
+// 		// Currently the keyword is only an integer.. This isn't gonna work :(
+// 		// databaseQuery = "SELECT * FROM filtered_relations WHERE keyword='$1'::VARCHAR";
+// 		// databaseQueryArray = [keyword];
+// 	}
+
+// 	// fetchFromPostgres(databaseQuery, databaseQueryArray, ...);
+
 // });
 
-pg.connect(connParam, function(err, client, done) {
-	if (err) {
-		return console.error('error fetching client from pool', err);
-	}
-	client.query('SELECT * FROM filtered_relations;', [], function (err, result) {
-		done();
+// HTTP Server
+var server = http.createServer(router);
+server.listen(httpPort);
 
-		if (err) {
-			return console.error('error running query', err);
+
+// function fetchFromPostgres(outstream) {
+// 	var databaseQuery = "SELECT * FROM filtered_relations";
+// 	var databaseQueryArray = [];
+
+// 	pg.connect(connParam, function (err, client, done) {
+
+// 		if (err) {
+// 			return console.error('error fetching client from pool', err);
+// 		}
+
+// 		var query = new QueryStream(databaseQuery, databaseQueryArray);
+// 		var stream = client.query(query);
+// 		stream.on('end', done);
+// 		stream.pipe(through(function (buf) {
+// 			// console.log(JSON.stringify(buf));
+// 			this.queue(JSON.stringify(buf));
+// 		}, function () {
+// 			this.queue(null);
+// 		})).pipe(outstream);
+		
+// 	});
+// }
+
+// fetchFromPostgres(process.stdout);
+
+
+// // Websocket Server
+// var someOtherServer = http.createServer();
+// websocket.createServer({server: someOtherServer}, fetchFromPostgres);
+// someOtherServer.listen(8081);
+
+var wsserver = nodejswebsocket.createServer(function (connection) {
+	connection.on("text", function (str) {
+		if (str === "fetch") {
+
+			var databaseQuery = "SELECT * FROM filtered_relations";
+			var databaseQueryArray = [];
+
+			pg.connect(connParam, function (err, client, done) {
+				if (err) throw err;
+				var query = new QueryStream(databaseQuery, databaseQueryArray);
+				var stream = client.query(query);
+
+				// stream.on("readable", function () {
+				// 	var buf = stream.read();
+				// 	console.log(JSON.stringify(buf));
+				// 	connection.sendText(JSON.stringify(buf));
+				// });
+				// stream.on("end", done);
+
+
+				stream.pipe(through(function (buf) {
+					console.log(JSON.stringify(buf));
+					connection.sendText(JSON.stringify(buf));
+				}, function () {
+					done();
+					connection.close();
+				}));
+				
+			});
 		}
-		result.rows.forEach(function (element, index, array) {
-			console.log('subject: ' + element.subject + ', '
-					  + 'object: ' + element.object + ', ' 
-					  + 'predicate: ' + element.predicate + ', '
-					  + 'keyword: ' + element.keyword);
-		});
-		client.end();
 	});
 });
-
-// var server = http.createServer(function (request, response) {
-// 	if (request.method != 'GET')
-// 		return response.end('Error: Not a GET');
-
-
-// });
-
-// server.listen(port);
+wsserver.listen(8081);
---
+++ static/scripts/websocket-client.js
@@ -0,0 +1,28 @@
+var connection;
+function websocketClient() {
+	connection = new WebSocket("ws://"+window.location.hostname+":8081");
+	connection.binaryType = "blob";
+	connection.onopen = function () {
+		connection.send("fetch");
+	};
+	connection.onclose = function () {
+		// console.log("Connection closed");
+		alert("Connection closed");
+	};
+	connection.onerror = function () {
+		console.error("Connection error");
+	};
+	connection.onmessage = function (event) {
+		// if (event.data instanceof Blob) {
+		// 	reader = new FileReader();
+		// 	reader.onload = function () {
+		// 		console.log(reader.result);
+		// 	}
+		// 	reader.readAsText(event.data);
+		// }
+		console.log(event.data);
+		var div = document.createElement("div");
+		div.textContent = event.data;
+		document.body.appendChild(div);
+	};
+}
