3186e594f41adcdf922f19481b30eed842d172e8
Michael Tu <tuzhucheng@outlook.com>
2015-03-21 17:04:28 -0400

    add verb frequency chart and highcharts

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -2,6 +2,19 @@ var s;
 var CANVAS_WIDTH = 900;
 var CANVAS_HEIGHT = 600;
 
+var filterKeywords = {
+    substance: {
+        proteins: [],
+        hormones: {
+            amine: [],
+            peptide: []
+        },
+        neurotransmitters: []
+    },
+    structure: {},
+    process: {},
+}
+
 var Node = function(id, name) {
     this.id = id;
     this.name = name;
@@ -33,6 +46,8 @@ var g = {
 };
 
 window.addEventListener('load', function() {
+    populateNodeFilterer();
+
     s = new sigma({
         graph: g,
         renderer: {
@@ -40,6 +55,11 @@ window.addEventListener('load', function() {
             type: 'canvas'
         },
         settings: {
+            minEdgeSize: 0.5,
+            maxEdgeSize: 4,
+            enableEdgeHovering: true,
+            edgeHoverSizeRatio: 1,
+            edgeHoverExtremities: true
         }
     });
 
@@ -47,8 +67,21 @@ window.addEventListener('load', function() {
         handleClickNode(e.data.node);
     });
 
+    s.bind('overEdge outEdge clickEdge', function(e) {
+        console.log('event', e);
+        handleClickEdge(e.data.edge);
+    });
+
+    s.bind('clickStage', function(e) {
+        handleClickStage();
+    });
+
     fetchRelations();
 });
+
+function populateNodeFilterer() {
+    // TODO populate the keywords in the top slider bar dynamically here
+}
 
 function fetchRelations() {
     websocketClient();
@@ -60,7 +93,7 @@ function getRandomInt(min, max) {
 
 function handleOnMessage(relation) {
 
-    if (nodes.length > 50) return;
+    if (nodes.length > 75) return;
     var subjectNode = _.find(nodes, function(node) {
         return node.name === relation.subject;
     });
@@ -118,6 +151,73 @@ function handleOnMessage(relation) {
 
 function handleClickNode(node) {
     $('#dp-name').text(node.label).show();
+    $('#dp-description').text('A description of ' + node.label + '.').show();
+
+    // find edges whose subject is this node
+    var outwardEdges = _.filter(edges, function(edge) {
+        return edge.source.toString() === node.id;
+    });
+
+    var groupedEdges = _.groupBy(outwardEdges, function(edge) {
+        return edge.name;
+    });
+
+    console.log('edges', outwardEdges);
+    console.log('groupedEdges', groupedEdges);
+
+    if (_.keys(groupedEdges).length > 0) {
+        $('#subject-verb-histogram').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Verb Frequency'
+            },
+            xAxis: {
+                categories: _.keys(groupedEdges),
+                title: {
+                    text: null
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Frequency',
+                    align: 'high'
+                },
+                labels: {
+                    overflow: 'justify'
+                }
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'Frequency',
+                data: _.map(groupedEdges, function(group) {
+                    return group.length;
+                })
+            }]
+        });
+    }
+}
+
+function handleClickEdge(edge) {
+    console.log(edge);
+}
+
+function handleClickStage() {
+
 }
 
 function applyLayout() {
