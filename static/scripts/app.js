c9c222f50cea3ffb07bbb0e6c2f7847f9b9781bf
Michael Tu <tuzhucheng@outlook.com>
2015-03-20 22:55:46 -0400

    display nodes on graph

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -23,41 +23,75 @@ _.extend(Edge.prototype, {
 
 var nodes = [];
 var edges = [];
+var s;
+var CANVAS_WIDTH = 900;
+var CANVAS_HEIGHT = 600;
 
 window.addEventListener('load', function() {
-var s = new sigma('graph-container');
+    s = new sigma('graph-container');
     s.settings({
         defaultEdgeColor: '#ec5148'
     });
-
-    // Then, let's add some data to display:
-    s.graph.addNode({
-      // Main attributes:
-      id: 'n0',
-      label: 'Hello',
-      // Display attributes:
-      x: 0,
-      y: 0,
-      size: 1,
-    }).addNode({
-      // Main attributes:
-      id: 'n1',
-      label: 'World !',
-      // Display attributes:
-      x: 1,
-      y: 1,
-      size: 1,
-    }).addEdge({
-      id: 'n0',
-      // Reference extremities:
-      source: 'n0',
-      target: 'n1'
-    });
-
-    // Finally, let's ask our sigma instance to refresh:
-    s.refresh();
 });
 
 function fetchRelations() {
     websocketClient();
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function handleOnMessage(relation) {
+    var subjectNode = _.find(nodes, function(node) {
+        return node.name === relation.subject;
+    });
+
+    if (!subjectNode) {
+        subjectNode = new Node(nodes.length, relation.subject);
+        nodes.push(subjectNode);
+        s.graph.addNode({
+          id: subjectNode.id.toString(),
+          label: subjectNode.name,
+          x: getRandomInt(0, CANVAS_WIDTH),
+          y: getRandomInt(0, CANVAS_HEIGHT),
+          size: 1,
+        });
+    }
+
+    var objectNode = _.find(nodes, function(node) {
+        return node.name === relation.object;
+    });
+
+    if (!objectNode) {
+        objectNode = new Node(nodes.length, relation.object);
+        nodes.push(objectNode);
+        s.graph.addNode({
+          id: objectNode.id.toString(),
+          label: objectNode.name,
+          x: getRandomInt(0, CANVAS_WIDTH),
+          y: getRandomInt(0, CANVAS_HEIGHT),
+          size: 1,
+        });
+    }
+
+    var edge = _.find(edges, function(edge) {
+        return edge.source === subjectNode.id
+                && edge.target === objectNode.id 
+                && edge.name === relation.predicate;
+    });
+
+    if (!edge) {
+        edge = new Edge(edges.length, subjectNode.id, objectNode.id, relation.predicate);
+        edges.push(edge);
+        s.graph.addEdge({
+          id: edge.id.toString(),
+          source: edge.source.toString(),
+          target: edge.target.toString()
+        });
+    }
+
+    edge.addLink(relation.link);
+
+    s.refresh();
 }
--- static/scripts/websocket-client.js
+++ static/scripts/websocket-client.js
@@ -13,37 +13,6 @@ function websocketClient() {
         console.error("Connection error");
     };
     connection.onmessage = function (event) {
-        var relation = JSON.parse(event.data);
-
-        var subjectNode = _.find(nodes, function(node) {
-            return node.name === relation.subject;
-        });
-
-        if (!subjectNode) {
-            subjectNode = new Node(nodes.length, relation.subject);
-            nodes.push(subjectNode);
-        }
-
-        var objectNode = _.find(nodes, function(node) {
-            return node.name === relation.object;
-        });
-
-        if (!objectNode) {
-            objectNode = new Node(nodes.length, relation.object);
-            nodes.push(objectNode);
-        }
-
-        var edge = _.find(edges, function(edge) {
-            return edge.source === subjectNode.id
-                    && edge.target === objectNode.id 
-                    && edge.name === relation.predicate;
-        });
-
-        if (!edge) {
-            edge = new Edge(edges.length, subjectNode.id, objectNode.id, relation.predicate);
-            edges.push(edge);
-        }
-
-        edge.addLink(relation.link);
+        handleOnMessage(JSON.parse(event.data));
     };
 }
