a8d1ecb557f35597329fc6c4799fee9f26f4a16c
Michael Tu <tuzhucheng@outlook.com>
2015-03-25 23:33:38 -0400

    remove automatic forceatlas because we have clustering

--- static/libs/bootstrap-slide-panel-top/main.js
+++ static/libs/bootstrap-slide-panel-top/main.js
@@ -13,13 +13,13 @@ $(document).ready(function() {
     // Button close panel
     $('#home-accordion .close').on('click', function() {
         $('#collapseOne').collapse('hide');
-        applyLayout();
-        window.setTimeout(stopLayout, 5000);
+        //applyLayout();
+        //window.setTimeout(stopLayout, 5000);
     });
 
     $('#bionavist-top-bar').on('hidden.bs.collapse', function () {
-        applyLayout();
-        window.setTimeout(stopLayout, 5000);
+        //applyLayout();
+        //window.setTimeout(stopLayout, 5000);
     });
 
 });
