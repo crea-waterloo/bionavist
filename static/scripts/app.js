1cfa6e9a297bd7e3ee86a726aaaca47e88be3498
Teyden Nguyen <teydenn@gmail.com>
2015-03-27 23:51:42 -0400

    Update app.js

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -5,24 +5,28 @@ var CANVAS_WIDTH = 900,
 
 var filterKeywords = {
     substance: {
-        proteins: ['actin', 'myosin', 'myoglobin', 'hemoglobin', 'melanopsin', 'photoisomerase', 'opsin', 'collagen', 'adiponectin', 'tropoelastin', 'elasin'],
-        hormones: {
+        protein: ['actin', 'myosin', 'myoglobin', 'hemoglobin', 'melanopsin', 'photoisomerase', 'opsin', 'collagen', 'adiponectin', 'tropoelastin', 'elastin'],
+        hormone: {
             amine: ['insulin', 'oxytocin', 'cortisol', 'glucagon', 'neuroestrogen'],
             peptide: ['progesterone', 'estrogen', 'testosterone', 'growth hormone', 'growth factor']
         },
-        neurotransmitters: ['dopamine', 'serotonin', 'norepinephrine', 'epinephrine', 'acetylcholine', 'GABA', 'catechcholamine']
+        neurotransmitter: ['dopamine', 'serotonin', 'norepinephrine', 'epinephrine', 'acetylcholine', 'GABA', 'catechcholamine']
     },
     structure: {
-        'Biological Systems': ['immune system', 'limbic system', 'circulatory system', 'digestive system', 'integumentary system', 'lymphatic system'],
-        'Tissues/Organs': ['amygdala', 'hypothalamus', 'thalamus', 'hippocampus', 'pituitary gland', 'adrenal gland', 'pancreas', 'acruate nucleus', 'kidney', 'blood vessels', 'esophagus', 'tumor'],
-        'Cells': ['lymphocyte', 'native T cell', 'helper T cell', 'cytotoxic T cell', 'activated T cell', 'rested T cells', 'spermatocyte', 'leukocyte', 'adipocyte', 'neuron', 'astrocyte', 'stem cell']
+        'Biological System': ['immune system', 'limbic system', 'circulatory system', 'digestive system', 'integumentary system', 'lymphatic system'],
+        'Tissue | Organ': ['amygdala', 'hypothalamus', 'thalamus', 'hippocampus', 'pituitary gland', 'adrenal gland', 'pancreas', 'acruate nucleus', 'kidney', 'blood vessels', 'esophagus', 'tumor'],
+        'Cell Type': ['lymphocyte', 'native T cell', 'helper T cell', 'cytotoxic T cell', 'activated T cell', 'rested T cells', 'spermatocyte', 'leukocyte', 'adipocyte', 'neuron', 'astrocyte', 'stem cell'],
+        'Receptor': ['G-protein coupled receptor', 'calcium ion channel', 'transport channel', 'serotonin receptor']
     },
     process: {
-        'Bio-Phenomena': ['apoptosis', 'glycolysis', 'glycogenolysis', 'glycolysis', 'lipolysis', 'embryogenesis'],
-        'Physiological': ['digestion', 'metabolism', 'heart rate', 'vasodilation', 'systolic blood pressure', 'diasystolic blood pressure'],
-        'Cognitive': ['memory', 'attention', 'arousal', 'intelligence'],
-        'Disease': ['cancer', 'diabetes', 'Parkinson\'s Disease', 'Huntington\'s Disease'],
-        'Other': ['light', 'stress', 'sleepiness', 'arousal', 'obesity', 'hunger']
+        'Molecular event': ['DNA synthesis', 'transcription', 'translation', 'DNA damage', 'DNA repair'] 
+        'Pathway event': ['[X] reuptake', 'MAPKKK cascade', 'signal transduction', '[X] binding', '[X] transport']
+        'Biological Phenomena': ['necrosis', apoptosis', 'glycolysis', 'glycogenolysis', 'glycolysis', 'lipolysis', 'embryogenesis', 'spermatogenesis'],
+        'Physiological': ['digestion', 'metabolism', 'heart rate', 'vasodilation', 'blood pressure'],
+        'Cognitive': ['memory', 'memory loss', 'attention', 'intelligence', 'executive function', 'perception'],
+        'Disease/Disorder': ['ebola', 'cancer', 'diabetes', 'ADHD', 'Parkinson\'s Disease', 'Huntington\'s Disease', 'obesity', 'anorexia', 'drug addiction', 'alcoholism'],
+        'Environmental event': ['drug administration', 'X injection', 'drug use', 'food intake', 'sun exposure', 'smoke inhalation']
+        'Other': ['light', 'dark', 'stress', 'sleepiness', 'arousal', 'hunger', 'fear', 'hearing', 'eye-sight', 'bone-formation', 'cell growth', 'death', 'aging', 'skin degeneration', 'thinning']
     },
 }
 
